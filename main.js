f31005709d353670c816fe5a643828a5f2d0b33a
TJacka <vegozzy@gmail.com>
2022-05-13 17:52:57 -0700

    Updated

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
+const stringEntry = document.getElementById("inputValue").value;
+console.log(stringEntry);
+
 const data = {
-    prompt: "Write a poem about a dog wearing skis",
+    prompt: stringEntry,
     temperature: 0.5,
     max_tokens: 64,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
    };
-    
+
    fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.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}`,
+      Authorization: `Bearer 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`,
     },
     body: JSON.stringify(data),
-   });
-   
+   })
+   .then(res => res.json())
+    .then(data => {
+        document.querySelector('li').append(data.choices[0].text)
+    })
+    .catch(error => {
+    console.log(error)
+    });
