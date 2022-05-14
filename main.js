f86bec2b56783a1781ca8011c4c541f2ee85a6a8
TJacka <vegozzy@gmail.com>
2022-05-14 10:38:52 -0700

    Working MVP complete

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-const stringEntry = document.getElementById("inputValue").value;
-console.log(stringEntry);
+document.querySelector('button').addEventListener('click', getStory);
 
-const data = {
-    prompt: stringEntry,
-    temperature: 0.5,
-    max_tokens: 64,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-   };
+function getStory() {
 
-   fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer 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`,
-    },
-    body: JSON.stringify(data),
+  let inputString = document.querySelector('textarea').value;
+
+  const data = {
+      prompt: `${inputString}`,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+  };
+
+  fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+   method: "POST",
+   headers: {
+     "Content-Type": "application/json",
+     Authorization: `Bearer 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`,
+   },
+   body: JSON.stringify(data),
    })
    .then(res => res.json())
-    .then(data => {
-        document.querySelector('li').append(data.choices[0].text)
-    })
-    .catch(error => {
-    console.log(error)
-    });
+   .then(data => {
+    console.log(data)  
+    document.querySelector("ul").innerText = data.choices[0].text
+      })
+   .catch(error => {
+   console.log(error)
+   });
+}
